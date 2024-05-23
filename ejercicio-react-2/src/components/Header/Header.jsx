33d7279e7accc77e2ea564d3cee5963c37273092
sentobc13 <sentoberlanga@gmail.com>
2024-05-23 17:08:37 +0200

    css header

--- ejercicio-react-2/src/components/Header/Header.jsx
+++ ejercicio-react-2/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom"
+import './Header.css';
 
 const Header = () => {
   return (
     <nav className="menu">
       <Link to="/">Home</Link>
-      <Link to="/reserva">Rserva</Link>
+      <Link to="/reserva">Reserva</Link>
       <Link to="/sobrenosotros">Sobre Nostros</Link>
     </nav>
   )
